f353b1303667130c4897b4c48383d3ecb48d7909
benamara <fatahbenamara87@hotmail.fr>
2020-07-02 01:50:52 +0200

    blocJS

---
+++ jr5/bloc/script.js
@@ -0,0 +1,29 @@
+/*HEADER */
+let textMenu="<img src=\"  http://via.placeholder.com/100x100  \">";
+textMenu += "<ul>";
+textMenu += "<li>Accueil</li>";
+textMenu += "<li>Connexion</li>";
+textMenu += "<li>Inscription</li>";
+textMenu += "</ul>";
+
+document.querySelector("header").innerHTML=textMenu;
+
+/* BODY */
+let textMain ="";
+for (let i = 0; i < 10; i++) {
+    
+    textMain += "<div>";
+    textMain += "<h2> Titre Article </h2>";
+    textMain += "<img src=\"  http://via.placeholder.com/300x300  \">";
+    textMain += "<p>lorem ipsum </p>";
+    textMain += "</div>";
+}
+
+document.querySelector("div").innerHTML = textMain;
+
+/*FOOTER */
+let textFooter= "Fatah "+ (new Date()).getFullYear();
+
+document.querySelector("footer").innerHTML=textFooter;
+
+
